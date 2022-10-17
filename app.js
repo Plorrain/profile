914e240b2322d0eac7444e9ad25f2876c7c503d0
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-17 12:44:53 -0400

    letters appearing 1 by 1

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ for(let i=0; i < splitText.length; i++){
   text.innerHTML += '<span>' + splitText[i] + '</span>'
 }
 
-console.log(splitText);
-
 let char = 0;
 let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++
+
+  if(char === splitText.length) {
+    complete();
+    return;
+  }
+
+  function complete() {
+    clearInterval(timer);
+    timer = null;
+  }
+}
